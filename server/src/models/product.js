8718b6b0148b987c9544d375e53f261d9d8bbfe7
tuanpa03 <phamanhtuan2833@gmail.com>
2023-05-31 20:05:35 +0700

    server: update model

--- server/src/models/product.js
+++ server/src/models/product.js
@@ -5,7 +5,9 @@ const Product = new Schema(
   {
     name: { type: String, required: true, text: true },
     price: { type: Number, required: true, min: 0 },
-    image: [{ type: Object, required: true }],
+    image: { type: String, required: true },
+    album: [{ type: String, required: true }],
+    quantity: { type: Number, required: true, min: 0 },
     description: { type: String, required: true },
     categoryId: { type: Types.ObjectId, ref: "Category", required: true },
     createdAt: { type: Date },
