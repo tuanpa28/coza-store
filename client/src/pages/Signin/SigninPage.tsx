7045720ea0b373d60a0ecd28623a223a7e756c2a
tuanpa03 <phamanhtuan2833@gmail.com>
2023-07-11 10:14:18 +0700

    client: update signin

--- client/src/pages/Signin/SigninPage.tsx
+++ client/src/pages/Signin/SigninPage.tsx
@@ -1,10 +1,9 @@
 import Cookies from "js-cookie";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, Navigate } from "react-router-dom";
 import { signin } from "../../api/auth";
 import IUser from "../../interfaces/auth";
 import { LockOutlined, MailOutlined } from "@ant-design/icons";
 import { Button, Form, Input, Typography, message, notification } from "antd";
-
 
 const SigninPage = () => {
   const navigate = useNavigate();
@@ -17,7 +16,7 @@ const SigninPage = () => {
       });
       message.success("Đăng nhập thành công!");
       navigate("/admin");
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch ({ response }: any) {
       notification.error({
         message: response.data.message,
@@ -25,52 +24,56 @@ const SigninPage = () => {
     }
   };
 
-
   return (
-    <Form
-      name="basic"
-      style={{ maxWidth: 350, margin: "0 auto", marginTop: 100 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      autoComplete="off"
-    >
-      <Typography.Title level={2}>Đăng nhập</Typography.Title>
-      <Form.Item
-        name="email"
-        rules={[{ required: true, message: "Vui lòng nhập Email!" }]}
-      >
-        <Input
-          type="email"
-          size="large"
-          prefix={<MailOutlined />}
-          placeholder="Email"
-        />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: "Vui lòng nhập Password!" }]}
-      >
-        <Input.Password
-          size="large"
-          prefix={<LockOutlined />}
-          placeholder="Password"
-        />
-      </Form.Item>
-
-      <Form.Item>
-        <Button
-          size="large"
-          // type="primary"
-          htmlType="submit"
-          className="login-form-button"
-          style={{ width: "100%" }}
+    <>
+      {Cookies.get("accessToken") ? (
+        <Navigate to="/admin" />
+      ) : (
+        <Form
+          name="basic"
+          style={{ maxWidth: 350, margin: "0 auto", marginTop: 100 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="off"
         >
-          Đăng nhập
-        </Button>
-     
-        Or <Link to={"/signup"}>register now!</Link>
-      </Form.Item>
-    </Form>
+          <Typography.Title level={2}>Đăng nhập</Typography.Title>
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: "Vui lòng nhập Email!" }]}
+          >
+            <Input
+              type="email"
+              size="large"
+              prefix={<MailOutlined />}
+              placeholder="Email"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Vui lòng nhập Password!" }]}
+          >
+            <Input.Password
+              size="large"
+              prefix={<LockOutlined />}
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              size="large"
+              // type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              style={{ width: "100%" }}
+            >
+              Đăng nhập
+            </Button>
+            Or <Link to={"/signup"}>register now!</Link>
+          </Form.Item>
+        </Form>
+      )}
+    </>
   );
 };
 
